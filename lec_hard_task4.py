b70edc57baed89d2bef4f54dbd15d08d42fd4658
999999999nineenin <maxim.kim.1333@list.ru>
2025-10-27 17:56:49 +0000

    324234

--- lec_hard_task4.py
+++ lec_hard_task4.py
@@ -1,15 +1,10 @@
 a=int(input())
 b=2
-c=1
 d=[]
-while c>1:
-    d=c
-    c=c-1
 while a > 1:
-    if a%2==0:
+    if a%b==0:
         a=a/b
-        if c%d!=0:
-            c=c
-        else:
-            c+=1
-        b=2+c
+        d.append(b)
+    else:
+        b+=1
+print(d)
