9ce091bd173b03ffdb4fb4541440f5f0623321b0
999999999nineenin <maxim.kim.1333@list.ru>
2025-11-20 14:37:27 +0000

    54t447g

--- lec_from.py
+++
@@ -1,6 +0,0 @@
-from lec_my_module import a, b
-
-print(a * b)
-
-from lec_my_module import *
-print(c[2] * c[1])
--- lec_math.py
+++
@@ -1,7 +0,0 @@
-import math
-
-a = math.sin(3 * math.pi / 5)
-print(a)
-
-d = math.asin(math.tan(math.pi / 4)) * 180 / math.pi
-print(d)
