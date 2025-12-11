3775bd15274646138b1a6537d797bd0f0333b0fb
999999999nineenin <maxim.kim.1333@list.ru>
2025-12-11 14:36:59 +0000

    5y4tyht

--- lec_random.py
+++
@@ -1,9 +0,0 @@
-import random, os
-
-value = random.randint(1, 6)
-print('На кубике выпало:', value)
-#os.system('python lec_random.py')
-
-for i in range(3):
-    print(random.random())
-
--- lec_time.py
+++
@@ -1,8 +0,0 @@
-import time
-
-timer = time.time()
-for i in range(10):
-    print(i)
-    time.sleep(1) #секунды
-
-print(f'{time.time() - timer}, seconds')
