51449cf9bdf11ddaed31a64565237d517030815c
999999999nineenin <maxim.kim.1333@list.ru>
2026-01-29 14:53:53 +0000

    65756756

--- lec_task1.py
+++ lec_task1.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
 
 phi = np.linspace(0, 2*np.pi, 100)
-theta = np.linspace(0, np.pi, 100)
+theta = np.linspace(0, 2*np.pi, 100)
 
 x = np.outer(phi, np.cos(theta))
 y = np.outer(phi, np.sin(theta))
-z = np.outer(phi ** 2)
+z = np.outer(phi ** 2, np.ones(len(theta)))
 
 ax.plot_surface(x, y, z)
 
